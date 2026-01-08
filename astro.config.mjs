de5a5106184ce1d20926f362797968bb314a3d36
Cesar Asto Ore <46658641+cesarst93@users.noreply.github.com>
2026-01-08 14:39:37 -0500

    New domain changed

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://cesarst93.github.io",
+  site: "https://cesarstevens.me",
   integrations: [preact(), icon(), sitemap({
     filter: (page) =>
       !page.includes("/blog/tags") &&
